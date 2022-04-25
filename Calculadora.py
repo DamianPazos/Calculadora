54d6ef3a372a55adbf95bd1e92beff7c149e4c98
Pazos Damian <dpazos@andreani.com>
2022-04-25 01:33:41 -0300

    Resta

--- Calculadora.py
+++ Calculadora.py
@@ -5,7 +5,8 @@ bandera_menu = True
 while (bandera_menu):
     print("Elige la opcion desesada: ")
     print("1. Suma")
-    print("2. Salir")
+    print('2. Resta')
+    print("3. Salir")
     opcion_menu = input("Ingrese el numero de la opcion: ")
     
     # Opcion N°1: Suma
@@ -15,5 +16,10 @@ while (bandera_menu):
         numero_total = numero_a + numero_b
         print(f'La suma es {numero_total}')
     elif (opcion_menu == "2"):
+        numero_a = float(input('Ingrese el primer valor a restar: '))
+        numero_b = float(input('Ingrese el segundo valor a restar: '))
+        numero_total = numero_a - numero_b
+        print(f'La resta es {numero_total}')
+    elif (opcion_menu == "3"):
         print('Eligio salir de la calculadora')
         bandera_menu = False
