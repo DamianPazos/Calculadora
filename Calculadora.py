d82906610dead05a1a5069a05c9fef39d95dbd58
Pazos Damian <dpazos@andreani.com>
2022-04-25 17:30:12 -0300

    Intefaz grafica Basica

--- Calculadora.py
+++ Calculadora.py
@@ -1,8 +1,7 @@
 from re import T
 import matplotlib.pyplot as plt
 import numpy as np
-from tkinter import Tk, Text
-
+from tkinter import Tk, Text, Button, END, re
 class Interfaz:
     def __init__(self,ventana):
         # Inicializo la ventana con su nombre
@@ -18,6 +17,65 @@ class Interfaz:
         #Inicializar la operacion mostrada en pantalla como un string vacio
         self.operacion =''
         
+        # Botones
+        boton_1 = self.crearBoton(1)
+        boton_2 = self.crearBoton(2)
+        boton_3 = self.crearBoton(3)
+        boton_4 = self.crearBoton(4)
+        boton_5 = self.crearBoton(5)
+        boton_6 = self.crearBoton(6)
+        boton_7 = self.crearBoton(7)
+        boton_8 = self.crearBoton(8)
+        boton_9 = self.crearBoton(9)
+        boton_0 = self.crearBoton(0)
+        boton_suma = self.crearBoton('+')
+        boton_resta = self.crearBoton('-')
+        boton_multiplicacion = self.crearBoton('x')
+        boton_division = self.crearBoton('/')
+        boton_igual = self.crearBoton('=',escribir=False)
+        boton_decimal = self.crearBoton('.')
+        boton_borrar = self.crearBoton('C',escribir=False)
+        
+        # Ubicacion de Botones
+        
+        botones = [boton_0,boton_1,boton_2,boton_3,boton_4,boton_5,boton_6,boton_7,boton_8,boton_9,boton_suma,boton_resta,boton_multiplicacion,boton_division,boton_igual,boton_decimal,boton_borrar]
+        contador = 0
+        for fila in range(1,5):
+            for columna in range (4):
+                botones[contador].grid(row=fila,column=columna)
+                contador +=1
+                
+    def crearBoton(self,valor,escribir=True,ancho=9,alto=1):
+        return Button(self.ventana,text=valor,width=ancho,height=alto,font=('Arial',15),command=lambda:self.click(valor,escribir))
+        
+    def click(self,texto,escribir):
+        if not escribir:
+            if texto == '=' and self.operacion!='':
+                resultado =str(eval(self.operacion))
+                self.operacion=''
+                self.limpiarPantalla()
+                self.mostrarEnPantalla(resultado)
+            elif texto == 'C':
+                self.operacion=''
+                self.limpiarPantalla()
+        # Mostrar texto
+        else:
+            self.operacion+=str(texto)
+            self.mostrarEnPantalla(texto)     
+        return
+    
+    def limpiarPantalla(self):
+        self.pantalla.configure(state='normal')
+        self.pantalla.delete('1.0',END)
+        self.pantalla.configure(state='disabled')
+        return
+    
+    def mostrarEnPantalla(self, valor):
+        self.pantalla.configure(state='normal')
+        self.pantalla.insert(END,valor)
+        self.pantalla.configure(state='disable')
+        return
+    
 root = Tk()
 calculadora = Interfaz(root)
 root.mainloop()
